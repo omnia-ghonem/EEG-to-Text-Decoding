c06133844d011097e4e06ed4c6b0292c007b5f32
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2024-11-10 00:30:35 +0200

    Delete .github/workflows/scripts.py

--- .github/workflows/scripts.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Access the GitHub secret from the environment
-secret_value = os.getenv("OPENAI_API_KEY ")
-
-if secret_value:
-    print(f"Secret successfully retrieved:{secret_value }")
-else:
-    print("Secret not found. Ensure it's set in the GitHub Actions secrets.")
