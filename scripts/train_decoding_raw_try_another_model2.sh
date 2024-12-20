1b5f1a5148b72f8b6dc4c0e6158dfc350835c60c
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2024-12-20 23:55:48 +0200

    Update train_decoding_raw_try_another_model2.sh

--- scripts/train_decoding_raw_try_another_model2.sh
+++ scripts/train_decoding_raw_try_another_model2.sh
@@ -1,11 +1,11 @@
 python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw_try_another_model2.py --model_name BrainTranslator \
     --task_name task1_task2_taskNRv2 \
-    --two_step \
+    --one_step \
     --pretrained \
-    --not_load_step1_checkpoint \
+    --load_step1_checkpoint \
     --not_first_run \
     --num_epoch_step1 10 \
-    --num_epoch_step2 2 \
+    --num_epoch_step2 15 \
     -lr1 0.00005 \
     -lr2 0.00005 \
     -b 20\
