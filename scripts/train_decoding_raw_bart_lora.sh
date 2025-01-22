258d03b29e24d78af6398bc8ea53c7ee19a2dc76
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2025-01-22 16:46:59 +0200

    Update train_decoding_raw_bart_lora.sh

--- scripts/train_decoding_raw_bart_lora.sh
+++ scripts/train_decoding_raw_bart_lora.sh
@@ -4,7 +4,7 @@ python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw_bart_lora.py --mo
     --pretrained \
     --not_load_step1_checkpoint \
     --first_run \
-    --num_epoch_step1 1 \
+    --num_epoch_step1 10 \
     --num_epoch_step2 1 \
     -lr1 0.00005 \
     -lr2 0.00005 \
