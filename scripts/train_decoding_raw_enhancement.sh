7d7c511d62ff1920d903b0b6b6fdb70f8081a41d
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2025-01-25 23:32:32 +0200

    Update train_decoding_raw_enhancement.sh

--- scripts/train_decoding_raw_enhancement.sh
+++ scripts/train_decoding_raw_enhancement.sh
@@ -4,7 +4,7 @@ python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw_enhancement_GANS.
     --pretrained \
     --not_load_step1_checkpoint \
     --first_run \
-    --num_epoch_step1 5 \
+    --num_epoch_step1 10 \
     --num_epoch_step2 5 \
     -lr1 0.00005 \
     -lr2 0.00005 \
