24a86a48f8bde1b8a4c28eb4130c1f4bec765fae
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2025-01-27 07:44:53 +0200

    Update train_decoding_raw_enhancement_GANS.py

--- train_decoding_raw_enhancement_GANS.py
+++ train_decoding_raw_enhancement_GANS.py
@@ -249,21 +249,20 @@ if __name__ == '__main__':
 
     # Load datasets
     whole_dataset_dicts = []
+
     if 'task1' in task_name:
         with open('/kaggle/input/dataset/ZuCo/task1-SR/pickle/task1-SR-dataset_wRaw.pickle', 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
-    '''
     if 'task2' in task_name:
         with open('/kaggle/input/dataset2/task2-NR-dataset_wRaw.pickle', 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
-
     if 'taskNRv2' in task_name:
         with open('/kaggle/input/dataset3/task2-NR-2.0-dataset_wRaw.pickle', 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
 
     with open(f'{CONFIG_DIR}/{save_name}.json', 'w') as out_config:
         json.dump(args, out_config, indent=4)
-    '''
+
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
 
     # Create datasets
