a04045913d5ead561b36c2b521956e6bf2486933
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2025-01-29 07:12:24 +0200

    Update train_decoding_raw_new_dataset.sh

--- scripts/train_decoding_raw_new_dataset.sh
+++ scripts/train_decoding_raw_new_dataset.sh
@@ -1,4 +1,6 @@
-python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw_new_dataset.py --model_name BrainTranslator \
+
+python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw_new_dataset.py\
+    --model_name BrainTranslator \
     --task_name handwriting \
     --two_step \
     --pretrained \
@@ -8,6 +10,12 @@ python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw_new_dataset.py --
     --num_epoch_step2 10 \
     -lr1 0.00005 \
     -lr2 0.00005 \
-    -b 20\
+    -b 20 \
     -s /kaggle/working/checkpoints/decoding_raw \
-    -cuda cuda:0
+    -cuda cuda:0 \
+    --data_dir /kaggle/working/handwritingBCI \
+    --hidden_dim 512 \
+    --embedding_dim 1024 \
+    --save_every 5 \
+    --patience 5 \
+    --num_workers 4
